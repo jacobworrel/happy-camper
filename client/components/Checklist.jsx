51b1dceebd79b655b13eefffceb21bcaf23b09fc
jacobworrel <jacobworrel@gmail.com>
2017-07-14 11:48:39 -0700

    refactor to stateless functional components

--- client/components/Checklist.jsx
+++ client/components/Checklist.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Item from './Item.jsx'
 
-export default class Checklist extends React.Component {
-
-  render() {
-    let items = [];
-    for (let i = 0; i < this.props.items.length; i += 1) {
-      items.push(<Item item={this.props.items[i]} removeItem={this.props.removeItem} category={this.props.category}/>)
-    }
-    return (
-      <ul>
-        <h3>{this.props.category}:</h3>
-        {items}
-      </ul>
-   );
+const Checklist = (props) => {
+  const items = [];
+  for (let i = 0; i < props.items.length; i += 1) {
+    items.push(<Item item={props.items[i]} removeItem={props.removeItem} category={props.category}/>)
   }
+  return (
+    <ul>
+      <h3>{props.category}:</h3>
+      {items}
+    </ul>
+ );
 }
+
+export default Checklist;
--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
 
-export default class Item extends React.Component {
-
-  render() {
-    return (
-      <li className='item'>
-        <input type="checkbox" />
-        <span className='item-name'>{this.props.item}</span>
-        <button className='delete-btn' onClick={() => this.props.removeItem(this.props.item, this.props.category)}>delete</button>
-      </li>
-   );
-  }
+const Item = (props) => {
+  return (
+    <li className='item'>
+      <input type="checkbox" />
+      <span className='item-name'>{props.item}</span>
+      <button className='delete-btn' onClick={() => props.removeItem(props.item, props.category)}>delete</button>
+    </li>
+ );
 }
+
+export default Item;
